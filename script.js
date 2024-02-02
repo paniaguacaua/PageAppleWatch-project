e6dd22170e8d2b44afde3164a8318e5de3aaae2c
Cauã Paniagua <paniaguacaua@gmail.com>
2024-02-02 05:08:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+//VARIÁVEIS:
+const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const tituloProduto = document.getElementById('titulo-produto');
+const nomeCor = document.getElementById('nome-cor-selecionada');
+const miniaturaImagem0 = document.getElementById('0-imagem-miniatura');
+const miniaturaImagem1 = document.getElementById('1-imagem-miniatura');
+const miniaturaImagem2 = document.getElementById('2-imagem-miniatura');
+
+//CORES:
+const verdeCipreste = {
+    nome: 'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+    nome: 'Azul-inverno',
+    pasta: 'imagens-azul-inverno'
+};
+
+const meiaNoite = {
+    nome: 'Meia-noite',
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+    nome: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
+//LISTAS:
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro]
+const opcoesTamanho = ['41 mm', '45 mm'];
+
+//VARIÁVEIS:
+let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
+let corSelecionada = 1;
+
+//FUNÇÕES:
+function trocarImagem() {
+    //MUDAR A OPÇÃO MINIATURAS EXIBIDAS:
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    imagemVisualizacao.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-' + imagemSelecionada + '.jpeg';
+}
+
+function trocarTamanho() {
+    //ATUALIZAR VARIÁVEL DE CONTROLE DO TAMANHO DA CAIXA:
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+    //MUDAR O TÍTULO DO PRODUTO:
+    tituloProduto.innerText = 'Pulseira loop esportiva ' + opcoesCores[corSelecionada].nome + ' para caixa de ' + opcoesTamanho[tamanhoSelecionado];
+    //MUDAR O TAMANHO DA IMAGEM DE ACORDO COM A OPÇÃO:
+    if (opcoesTamanho[tamanhoSelecionado] === '41 mm'){
+        imagemVisualizacao.classList.add('caixa-pequena');
+    } else {
+        imagemVisualizacao.classList.remove('caixa-pequena')
+    }
+}
+
+function trocarCor() {
+    //ATUALIZAR COR SELECIONADA:
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
+    corSelecionada = idOpcaoSelecionada.charAt(0);
+    //TROCAR TÍTULO DA PÁGINA:
+    tituloProduto.innerText = 'Pulseira loop esportiva ' + opcoesCores[corSelecionada].nome + ' para caixa de ' + opcoesTamanho[tamanhoSelecionado];
+    //TROCAR NOME DA COR:
+    nomeCor.innerText = 'Cor - ' + opcoesCores[corSelecionada].nome;
+    //TROCAR IMAGENS MINIATURAS EXIBIDAS:
+    miniaturaImagem0.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-0.jpeg';
+    miniaturaImagem1.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-1.jpeg';
+    miniaturaImagem2.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-2.jpeg';
+    //TROCAR IMAGENS DE VISUALIZAÇÃO:
+    imagemVisualizacao.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-' + imagemSelecionada + '.jpeg';
+}
